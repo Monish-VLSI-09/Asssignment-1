07f546c834013018f4b2cb97cc1afd8018b066a7
Monish-VLSI-09 <167014630+Monish-VLSI-09@users.noreply.github.com>
2024-06-01 21:13:44 +0530

    Update T_FF.v

--- T_FF.v
+++ T_FF.v
@@ -1,1 +1,29 @@
-. Design T Flip Flop using Latches having asynchronous set and clear
+// Design T Flip Flop having asynchronous set and clear//
+
+
+module t_ff (
+  input T,
+  input S,
+  input C,
+  input clk,
+  output reg Q
+);
+
+  wire Qbar;
+  wire D;
+
+  assign D = T ^ Q;
+  
+  always @(posedge clk or posedge C or posedge S)
+  begin
+    if (C)
+      Q <= 1'b0;
+    else if (S)
+      Q <= 1'b1;
+    else
+      Q <= D;
+  end
+
+  assign Qbar = ~Q;
+
+endmodule
